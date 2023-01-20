cfe33b85c746ca6fabb1c9c9d2310e8d4471b79d
pl4cydo <placydo.doc@gmail.com>
2023-01-19 22:47:31 -0300

    pŕimeiro end point feito com metodo get

--- Haku/src/main/java/com/project/Haku/Controller/PokeController.java
+++ Haku/src/main/java/com/project/Haku/Controller/PokeController.java
@@ -1,11 +1,13 @@
 package com.project.Haku.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.Haku.Class.PokeMoves;
 import com.project.Haku.Class.Pokemon;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class PokeController {
     @GetMapping("/pokemon")
